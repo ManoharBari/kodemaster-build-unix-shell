b86176c4d6caceafaed46830c1c1c9bfe3a3df9c
Manohar Kale <manoharkale5368@gmail.com>
2026-01-19 19:08:36 +0530

    feat: implement exit builtin command

--- main.c
+++ main.c
@@ -3,105 +3,95 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <errno.h>
 
 #define BUFFER_SIZE 1024
 #define MAX_ARGS 64
 #define DELIMITERS " \t\r\n"
 
-/* Parse input line into arguments */
-void parse_line(char *line, char **args) {
+char **parse_line(char *line) {
+    static char *args[MAX_ARGS];
     int i = 0;
+    
     char *token = strtok(line, DELIMITERS);
-
     while (token != NULL && i < MAX_ARGS - 1) {
         args[i++] = token;
         token = strtok(NULL, DELIMITERS);
     }
     args[i] = NULL;
+    
+    return args;
 }
 
-/* Execute external command */
 int execute_external(char **args) {
     pid_t pid = fork();
-
+    
     if (pid == 0) {
-        execvp(args[0], args);
-
-        /* execvp returns only on error */
-        fprintf(stderr, "%s: command not found\n", args[0]);
-        _exit(127);
-    }
-    else if (pid > 0) {
+        if (execvp(args[0], args) == -1) {
+            fprintf(stderr, "%s: command not found\n", args[0]);
+            exit(127);
+        }
+    } else if (pid > 0) {
         int status;
-        if (waitpid(pid, &status, 0) == -1) {
-            perror("waitpid");
-        }
-
-        if (WIFEXITED(status)) {
-            return WEXITSTATUS(status);
-        }
-    }
-    else {
+        waitpid(pid, &status, 0);
+    } else {
         perror("fork");
     }
-
+    
     return 1;
 }
 
-/* Handle built-in commands */
 int handle_builtin(char **args) {
+    // exit [code]
     if (strcmp(args[0], "exit") == 0) {
         int exit_code = 0;
-
-        if (args[1]) {
-            char *endptr;
-            long val = strtol(args[1], &endptr, 10);
-
-            if (*endptr != '\0') {
-                fprintf(stderr, "exit: numeric argument required\n");
-                return 1;
-            }
-            exit_code = (int)val;
+        if (args[1] != NULL) {
+            exit_code = atoi(args[1]);
         }
-
         exit(exit_code);
     }
-
-    return -1; // Not a builtin
+    
+    // Not a builtin
+    return -1;
 }
 
 int execute(char **args) {
-    if (args[0] == NULL)
+    if (args[0] == NULL) {
         return 1;
-
-    int builtin_status = handle_builtin(args);
-    if (builtin_status != -1)
-        return builtin_status;
-
+    }
+    
+    // Try builtins first
+    int result = handle_builtin(args);
+    if (result != -1) {
+        return result;
+    }
+    
+    // Execute as external command
     return execute_external(args);
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
     char input[BUFFER_SIZE];
-    char *args[MAX_ARGS];
+    char **args;
     int status = 1;
-
+    
     while (status) {
         printf("$ ");
         fflush(stdout);
-
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        
+        if (fgets(input, BUFFER_SIZE, stdin) == NULL) {
             printf("\n");
             break;
         }
-
-        if (input[0] == '\n')
+        
+        input[strcspn(input, "\n")] = '\0';
+        
+        if (strlen(input) == 0) {
             continue;
-
-        parse_line(input, args);
+        }
+        
+        args = parse_line(input);
         status = execute(args);
     }
-
+    
     return 0;
 }
